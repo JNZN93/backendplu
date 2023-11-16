0f74191c72529374eba9efa8f4e5b33475b1088f
JNZN93 <firat.tasyurdu@gmail.com>
2023-11-17 00:33:46 +0100

    alles hochgeladen

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const fs = require('fs')
+app.use(cors())
+// localhost:3000
+const PORT = 3000 || process.env.PORT
+
+// um auf umgebunsvariable zuzugreifen
+require('dotenv').config()
+
+let globallist
+
+fs.readFile("plulist.json", 'utf8', (err, liste) => {
+    // Wenn Fehler auftaucht , Fehler anzeigen
+    if (err) {
+      console.error('Error reading JSON file:', err);
+      return;
+    // Wenn kein Fehler auftaucht 
+    }
+    else {
+        // Unsere Liste
+        globallist = liste
+    }
+  }
+)
+
+
+app.get("/", function(req,res){
+                // JSON OBJECT
+    res.json({ message:'Willkommen bei der PLU APP' })
+})
+
+// localhost:3000/plulist
+app.get('/plulist', function(req, res) {
+    // JSON ARRAY
+    console.log(globallist)
+    res.send(globallist); 
+  });
+
+
+app.listen(PORT, ()=> { console.log("SERVER LÄUFT auf PORT " + PORT) } )
